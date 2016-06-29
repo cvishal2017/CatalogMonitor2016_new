81fcd46171a508dfc4aa8cfe5960b531d192976a
snehalpansare <spansari@in.ibm.com>
2016-06-29 14:32:09 +0530

    add servlet xml

--- src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
+++ src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:*/applicationContext.xml" })
+@ContextConfiguration(locations = { "classpath:*/applicationContext.xml" , "classpath:*/monitor-servlet.xml"})
 public class ApplicationTest {
 	
 	@Autowired
