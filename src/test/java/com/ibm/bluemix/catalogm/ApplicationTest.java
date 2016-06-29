a69a14dddcfa5fb4e184c2afd5ab3c5bb1ef3f89
snehalpansare <spansari@in.ibm.com>
2016-06-29 14:26:07 +0530

    add app context xml

--- src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
+++ src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(locations = { "classpath:*/applicationContext.xml" })
 public class ApplicationTest {
 	
 	@Autowired
