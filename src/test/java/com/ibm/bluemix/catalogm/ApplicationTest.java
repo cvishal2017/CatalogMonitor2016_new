7d1c30866fcac1c1b98eb7681f203699893087b4
snehalpansare <spansari@in.ibm.com>
2016-06-29 17:02:01 +0530

    f

--- src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
+++ src/test/java/com/ibm/bluemix/catalogm/ApplicationTest.java
@@ -10,12 +10,11 @@ import static org.mockito.Mockito.atLeast;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:*/applicationContext.xml" , "classpath:*/monitor-servlet.xml"})
+@ContextConfiguration
 public class ApplicationTest {
 	
 	@Mock
